a27cb4d0ba1fbf51062cf1ec1c0625a90cace92b
PUM SUAN KHAI <101924004@student.swin.edu.au>
2022-08-11 23:21:23 +1000

    Update NavMenu

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -3,44 +3,89 @@ import { Navbar, Container, Row, Col, Button } from 'react-bootstrap';
 import Logo from '../../assets/images/easyshop.png'
 //import Logo from '../../assets/images/easylogon.jpeg'
 import { Link } from 'react-router-dom'
+import MegaMenuAll from '../home/MegaMenuAll';
 
 
 
 export class NavMenuDesktop extends Component {
+  constructor() {
+    super();
+    this.state = {
+      SideNavState: "sideNavClose",
+      ContentOverState: "ContentOverlayClose"
+    }
+  }
+
+  MenuBarClickHandler = () => {
+    this.SidNavOpenClose();
+  }
+
+  ContentOverlayClickHandler = () => {
+    this.SidNavOpenClose();
+  }
+
+
+  SidNavOpenClose = () => {
+    let SideNavState = this.state.SideNavState;
+    let ContentOverState = this.state.ContentOverState;
+    if (SideNavState === "sideNavOpen") {
+      this.setState({
+        SideNavState: "sideNavClose",
+        ContentOverState: "ContentOverlayClose"
+      })
+    } else {
+      this.setState({
+        SideNavState: "sideNavOpen",
+        ContentOverState: "ContentOverlayOpen"
+      })
+    }
+
+  }
+
   render() {
     return (
       <Fragment>
         <div className="TopSectionDown">
 
-          <Container fluid={true} className="fixed-top shawdow-sm p-2 mb-0 bg-white">
-            <Row>
-              <Col lg={4} md={4} sm={12} xs={12}>
-                <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
-              </Col>
+            <Container fluid={true} className="fixed-top shawdow-sm p-2 mb-0 bg-white">
+              <Row>
+                <Col lg={4} md={4} sm={12} xs={12}>
+                  <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i></Button>
+                  <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
+                </Col>
 
-              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                <div className="input-group w-100">
-                  <input type="text" className="form-control" />
-                  <Button type="button" className="btn site-btn"><i className="fa fa-search"> </i>
-                  </Button>
-                </div>
-              </Col>
+                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                  <div className="input-group w-100">
+                    <input type="text" className="form-control" />
+                    <Button type="button" className="btn site-btn"><i className="fa fa-search"> </i>
+                    </Button>
+                  </div>
+                </Col>
 
 
-              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                <Link to="/" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>
-                </Link>
-                <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
-                <Link to="/" className="h4 btn">LOGIN</Link>
+                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                  <Link to="/" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>
+                  </Link>
+                  <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
+                  <Link to="/" className="h4 btn">LOGIN</Link>
 
-                <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
-              </Col>
+                  <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
+                </Col>
 
-            </Row>
-          </Container>
+              </Row>
+            </Container>
 
 
         </div>
+        <div className={this.state.SideNavState}>
+          <MegaMenuAll />
+        </div>
+
+
+        <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
+
+        </div>
+
 
 
       </Fragment>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,33 @@
 import React, { Component, Fragment } from 'react'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeatureProduct from '../components/home/FeatureProduct'
 import HomeTop from '../components/home/HomeTop'
+import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <div className='Desktop'>
         <NavMenuDesktop />
         <HomeTop />
+        </div>
+
+
+        <div className='Mobile'>
+        <NavMenuMobile />
+        <HomeTopMobile />
+        </div>
+
         <FeatureProduct />
         <NewArrival />
         <Categories />
         <Collection />
-        
+
 
 
       </Fragment>
